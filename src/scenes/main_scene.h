ad28e35cef3c7619a04fc7661b785771bb11468b
gimploo <41667550+gimploo@users.noreply.github.com>
2025-04-24 21:52:05 +0530

    task: rendered a black cube
    Default shader used here i think was meant only for 2d shapes and not 3d
    objects - need to add another default shader for 3d next

--- src/scenes/main_scene.h
+++ src/scenes/main_scene.h
@@ -1,11 +1,20 @@
 #pragma once
 #include "poglib/basic.h"
+#include "../game.h"
 
-typedef struct game_t {
-} game_t;
+typedef struct {
+    glshader_t shader;
+} main_scene_t;
 
 void main_scene_init(struct scene_t * s)
 {
+    scene_pass_content(
+        s, 
+        &(main_scene_t ) {
+            .shader = glshader_default_init(),
+        }, 
+        sizeof(main_scene_t)
+    );
 }
 
 void main_scene_input(struct scene_t *s, const f32 dt)
@@ -18,8 +27,15 @@ void main_scene_update(struct scene_t *s, const f32 dt)
 
 void main_scene_render(struct scene_t *s)
 {
+    const main_scene_t *game = (const main_scene_t *)s->content;
+
+    glrenderer3d_draw_cube(&(glrenderer3d_t) {
+        .shader = &game->shader,
+    });
 }
 
 void main_scene_destroy(struct scene_t *s)
 {
+    main_scene_t *game = (const main_scene_t *)s->content;
+    glshader_destroy(&game->shader);
 }
